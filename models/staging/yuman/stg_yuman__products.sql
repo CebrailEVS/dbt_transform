807aa94c8a7cd4b6f3d9cd13f0374dece2f5c390
CebrailEVS <cebrail.aksoy@evs-pro.com>
2025-07-07 13:01:49 +0000

    ajout du modele stg product yuman

---
+++ models/staging/yuman/stg_yuman__products.sql
@@ -0,0 +1,32 @@
+{{ 
+  config(
+    materialized='table',
+    description='Produits/Articles nettoyés et enrichis depuis yuman_products',
+  ) 
+}}
+
+with source_data as (
+    select * 
+    from {{ source('yuman_api', 'yuman_products') }}
+),
+
+cleaned_products as (
+    select
+        id as product_id,
+        reference as product_code,
+        designation as product_name,
+        product_type as product_type,
+        brand as product_brand,
+        unit as product_unit,
+        purchase_price as purchase_price,
+        sale_price as sale_price,    
+        active as is_active,
+        TIMESTAMP(created_at) as created_at,
+        TIMESTAMP(updated_at) as last_updated,
+        TIMESTAMP(_sdc_extracted_at) as extracted_at,
+        TIMESTAMP(_sdc_deleted_at) as deleted_at
+    from source_data
+    where id is not null
+)
+
+select * from cleaned_products
