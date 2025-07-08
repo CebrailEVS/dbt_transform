75af86419aaae64ef5ed16a7002af7f8be5068ee
CebrailEVS <cebrail.aksoy@evs-pro.com>
2025-07-08 07:30:48 +0000

    ajout du staging/source user et workorder_demands

---
+++ models/staging/yuman/stg_yuman__users.sql
@@ -0,0 +1,29 @@
+{{
+  config(
+    materialized='table',
+    description='Utilisateurs nettoyés depuis l Yuman API',
+  )
+}}
+
+with source_data as (
+    select * from {{ source('yuman_api', 'yuman_users') }}
+),
+
+cleaned_users as (
+    select
+        id as user_id,
+        manager_id, -- ID du manager de l'utilisateur
+        agency_id,
+        name as user_name,
+        email as user_email,
+        user_type as user_type,
+        phone as user_phone,
+        manager_as_technician as is_manager_as_technician,
+        cast(created_at as timestamp) as created_at,
+        cast(updated_at as timestamp) as last_updated,
+        cast(_sdc_extracted_at as timestamp) as extracted_at,
+        cast(_sdc_deleted_at as timestamp) as deleted_at
+    from source_data
+)
+
+select * from cleaned_users
---
+++ models/staging/yuman/stg_yuman__workorder_demands.sql
@@ -0,0 +1,34 @@
+{{ 
+  config(
+    materialized='table',
+    description='Demande dintervention nettoyés et enrichis depuis yuman_workorder_demands',
+  ) 
+}}
+
+with source_data as (
+    select * 
+    from {{ source('yuman_api', 'yuman_workorder_demands') }}
+),
+
+cleaned_workorder_demdands as (
+    select
+        id as demand_id,
+        workorder_id,
+        material_id,
+        site_id,
+        client_id,
+        user_id,
+        cast(contact_id as int64) as contact_id,
+        category_id,
+        description as demand_description,
+        status as demand_status,
+        reject_comment,
+        TIMESTAMP(created_at) as created_at,
+        TIMESTAMP(updated_at) as last_updated,
+        TIMESTAMP(_sdc_extracted_at) as extracted_at,
+        TIMESTAMP(_sdc_deleted_at) as deleted_at
+    from source_data
+    where id is not null
+)
+
+select * from cleaned_workorder_demdands
