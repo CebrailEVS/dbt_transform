aa1fc1e4d9bf93446a3691728e2827d7e63f6b37
CebrailEVS <cebrail.aksoy@evs-pro.com>
2025-07-08 10:14:32 +0000

    update workorders models/sources

---
+++ models/staging/yuman/stg_yuman__workorders_categories.sql
@@ -0,0 +1,23 @@
+{{
+  config(
+    materialized='table',
+    description='Catégories dintervention nettoyés depuis l Yuman API',
+  )
+}}
+
+with source_data as (
+    select * from {{ source('yuman_api', 'yuman_workorder_categories') }}
+),
+
+cleaned_categories as (
+    select
+        id as category_id,
+        name as category_name,
+        cast(created_at as timestamp) as created_at,
+        cast(updated_at as timestamp) as last_updated,
+        cast(_sdc_extracted_at as timestamp) as extracted_at,
+        cast(_sdc_deleted_at as timestamp) as deleted_at
+    from source_data
+)
+
+select * from cleaned_categories
