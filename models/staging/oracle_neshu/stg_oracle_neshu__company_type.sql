99b3aa999150b3b06beb96fbe7db6932fbb048c0
CebrailEVS <cebrail.aksoy@evs-pro.com>
2025-07-21 09:55:09 +0000

    ajout modele yml de toutes les tables et construction modele company_type et contact

---
+++ models/staging/oracle_neshu/stg_oracle_neshu__company_type.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        materialized='table',
+        cluster_by=['idcompany_type'],
+        description='Type client nettoyés et enrichis depuis evs_company_type'
+    )
+}}
+
+with source_data as (
+    select *
+    from {{ source('oracle_neshu', 'evs_company_type') }}
+),
+
+cleaned_data as (
+    select
+        -- IDs convertis en BIGINT
+        cast(idcompany_type as int64) as idcompany_type,
+        
+        -- Colonnes texte
+        code,
+
+        -- Timestamps harmonisés
+        timestamp(modification_date) as updated_at,
+        timestamp(_sdc_extracted_at) as extracted_at,
+        timestamp(_sdc_deleted_at) as deleted_at
+        
+    from source_data
+)
+
+select * from cleaned_data
---
+++ models/staging/oracle_neshu/stg_oracle_neshu__contact.sql
@@ -0,0 +1,39 @@
+{{
+    config(
+        materialized='table',
+        cluster_by=['idcontact','idcompany'],
+        description='Contact nettoyés et enrichis depuis evs_company'
+    )
+}}
+
+with source_data as (
+    select *
+    from {{ source('oracle_neshu', 'evs_contact') }}
+),
+
+cleaned_data as (
+    select
+        -- IDs convertis en BIGINT
+        cast(idcontact as int64) as idcontact,
+        cast(idcompany as int64) as idcompany,
+        
+        -- Colonnes texte
+        code,
+        first_name,
+        last_name,
+        email,
+        name,
+        mobile,
+        phone,
+        qualite,
+        code_status_record,
+
+        -- Timestamps harmonisés
+        timestamp(modification_date) as updated_at,
+        timestamp(_sdc_extracted_at) as extracted_at,
+        timestamp(_sdc_deleted_at) as deleted_at
+        
+    from source_data
+)
+
+select * from cleaned_data
