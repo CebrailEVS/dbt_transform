6d9f3962d43c5e56865b706b45417ff09976e542
CebrailEVS <cebrail.aksoy@evs-pro.com>
2025-07-21 13:28:39 +0000

    added label resources/product staging

---
+++ models/staging/oracle_neshu/stg_oracle_neshu__label_has_resources.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized='table',
+        cluster_by=['idlabel'],
+        description='label_has_resources nettoyés et enrichis depuis evs_label_has_resources'
+    )
+}}
+
+with source_data as (
+    select *
+    from {{ source('oracle_neshu', 'evs_label_has_resources') }}
+),
+
+cleaned_data as (
+    select
+        -- IDs convertis en BIGINT
+        cast(idlabel as int64) as idlabel,
+        cast(idresources as int64) as idresources,
+
+        -- Timestamps harmonisés
+        timestamp(_sdc_extracted_at) as extracted_at,
+        timestamp(_sdc_deleted_at) as deleted_at
+        
+    from source_data
+)
+
+select * from cleaned_data
