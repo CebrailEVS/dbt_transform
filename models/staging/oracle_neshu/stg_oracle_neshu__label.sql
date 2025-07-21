bff07e24b6cbfdf6a46ceb06db15256a18979e78
CebrailEVS <cebrail.aksoy@evs-pro.com>
2025-07-21 13:15:08 +0000

    added label

---
+++ models/staging/oracle_neshu/stg_oracle_neshu__label.sql
@@ -0,0 +1,35 @@
+{{
+    config(
+        materialized='table',
+        cluster_by=['idlabel','idlabel_family'],
+        description='Label nettoyés et enrichis depuis evs_company'
+    )
+}}
+
+with source_data as (
+    select *
+    from {{ source('oracle_neshu', 'evs_label') }}
+),
+
+cleaned_data as (
+    select
+        -- IDs convertis en BIGINT
+        cast(idlabel as int64) as idlabel,
+        cast(idlabel_family as int64) as idlabel_family,
+        
+        -- Colonnes texte
+        code,
+
+        -- Bolean
+        cast(system as boolean) as is_system,
+        cast(enabled as boolean) as is_enabled,
+
+        -- Timestamps harmonisés
+        timestamp(modification_date) as updated_at,
+        timestamp(_sdc_extracted_at) as extracted_at,
+        timestamp(_sdc_deleted_at) as deleted_at
+        
+    from source_data
+)
+
+select * from cleaned_data
