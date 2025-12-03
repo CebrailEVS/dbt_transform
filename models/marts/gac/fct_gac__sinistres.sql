91c13dfc3bb1eaecc36f5eb4141e383364a34173
EtienneEVS <etienne.boulinier@evs-pro.com>
2025-12-03 10:49:03 +0100

    new model sinistres marts

---
+++ models/marts/gac/fct_gac__sinistres.sql
@@ -0,0 +1,38 @@
+{{ config(
+    materialized = "table",
+    schema='marts',
+    alias = "fct_gac_sinistres",
+    partition_by={},
+    cluster_by=[]
+) }}
+-- ============================================================================
+-- MODEL: fct_gac_sinistres
+-- PURPOSE: Table de faits sur les sinistres vehicules
+-- AUTHOR: Etienne BOULINIER
+-- ============================================================================
+SELECT 
+-- Infos Sinistre
+n_de_sinistre,
+DATE(date_sinistre) as date_sinistre,
+date_de_creation,
+circonstance,
+tiers,
+resp,
+cloture,
+-- Infos Vehicule
+immat,
+nom,
+prenom,
+statut_actuel,
+genre_fiscal,
+reference_gac,
+entite_entite_2,
+entite_entite_3,
+--Couts
+centre_de_couts,
+cout_assureur,
+auto_assurance,
+franchise,
+cout_global,
+cout_client
+ FROM ref('stg_gac__sinistres_sg')
