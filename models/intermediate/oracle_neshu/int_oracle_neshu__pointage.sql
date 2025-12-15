841a731db6f5f1bfb6f4ad9366835b2cb98ac5ae
CebrailEVS <cebrail.aksoy@evs-pro.com>
2025-12-15 15:07:13 +0000

    feat: ajout modele intermediaire des tâches pointage roadman neshu [skip ci]

--- models/intermediate/oracle_neshu/int_oracle_neshu__pointage.sql
+++ models/intermediate/oracle_neshu/int_oracle_neshu__pointage.sql
@@ -1,7 +1,11 @@
 {{
     config(
         materialized='table',
-        description='Table intermédiaire des tâches des pointages roadman sur les débuts de journées avec dédoublon sur un même jour et même roadman en prenant le premier pointage après 3h du matin de la journée => 1 ligne = 1 tâche pointage roadman par jour'
+        description='Table intermédiaire des tâches des pointages roadman sur les débuts & fin de journées filtré sur les tâches de type POINTAGE ' 
+                    'et filtré sur label START_DAY & END_DAY (idtask_type=194, idlabel in (2685,2686)) avec code_status_record=1. '
+                    'Chaque pointage est associé à une ressource roadman (idresources_type=2). '
+                    '1 ligne = 1 tâche pointage'
+                    'Filtrée pour ne garder que les tâches avec ressource roadman associée.'
     )
 }}
 
@@ -30,7 +34,7 @@ pointage AS (
         t.extracted_at
     FROM {{ ref('stg_oracle_neshu__task') }} t
     JOIN {{ ref('stg_oracle_neshu__label_has_task') }} lht ON t.idtask = lht.idtask
-    JOIN {{ ref('stg_oracle_neshu__label') }} la ON lht.idlabel = la.idlabel AND la.idlabel = 2685 -- START_DAY uniquement
+    JOIN {{ ref('stg_oracle_neshu__label') }} la ON lht.idlabel = la.idlabel AND la.idlabel in (2685,2686)  -- START_DAY & END_DAY uniquement
     LEFT JOIN {{ ref('stg_oracle_neshu__task_status') }} ts ON t.idtask_status = ts.idtask_status
     WHERE t.idtask_type = 194 -- TASK TYPE POINTAGE
       AND t.code_status_record = '1' -- Enregistrement actif
@@ -42,38 +46,9 @@ pointage_resources AS (
         r.code_roadman
     FROM pointage p
     LEFT JOIN resources_roadman r ON p.idtask = r.idtask
-),
--- Un seul pointage par jour et par roadman
-pointage_unique_par_jour AS (
-    SELECT
-        idtask,
-        idcompany,
-        idresources_roadman,
-        code_roadman,
-        status_code,
-        label_code,
-        DATE(date_pointage) AS date_jour,
-        created_at,
-        updated_at,
-        extracted_at,
-        MIN(CASE 
-            WHEN FORMAT_TIMESTAMP('%H:%M:%S', date_pointage) >= '03:00:00' THEN date_pointage
-        END) AS premier_pointage
-    FROM pointage_resources
-    GROUP BY idtask, idcompany, idresources_roadman, code_roadman, status_code, label_code, DATE(date_pointage), created_at, updated_at, extracted_at 
-),
-pointage_min_par_jour AS (
-    SELECT
-        p.*,
-        ROW_NUMBER() OVER (
-            PARTITION BY
-                p.idresources_roadman,
-                p.date_jour
-            ORDER BY
-                p.premier_pointage
-        ) AS rn
-    FROM pointage_unique_par_jour p
 )
+
+-- SELECT FINAL
 SELECT
     idtask as task_id,
     idcompany as company_id,
@@ -81,10 +56,10 @@ SELECT
     code_roadman as roadman_code,
     status_code,
     label_code,
-    date_jour as date_pointage_jour,
-    premier_pointage as date_pointage,
+    date_pointage as date_pointage,
+    DATE(date_pointage) as date_pointage_jour,
     created_at,
     updated_at,
     extracted_at
-FROM pointage_min_par_jour
-WHERE rn = 1 and idresources_roadman IS NOT NULL -- Garde uniquement les pointages avec roadman et dédoublonne en prenant le premier pointage du jour par roadman
+FROM pointage_resources
+WHERE idresources_roadman is not null
