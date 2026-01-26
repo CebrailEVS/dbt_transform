f8f93251a677b5249890294ba52a76881e52d7d2
EtienneEVS <etienne.boulinier@evs-pro.com>
2026-01-26 16:15:34 +0100

    Ajout de la description du modèle

--- models/marts/mssql_sage/mssql_sage__pnl_bu_kpis.sql
+++ models/marts/mssql_sage/mssql_sage__pnl_bu_kpis.sql
@@ -1,5 +1,6 @@
 {{ config(
-    materialized = 'table'
+    materialized = 'table',
+    description='Table avec aggregation par mois et année des différents KPIs (MS, CA, Charges, Marge Nette, MArge Brute...) utiles aux analyses P&L par BU - Table générée pour le BI P&L par BU'
 ) }}
 
 WITH scenarios AS (
