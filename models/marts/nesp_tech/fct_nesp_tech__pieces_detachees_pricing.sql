e0df8b121588e3afb36711a3bb442e862db70b10
EtienneEVS <etienne.boulinier@evs-pro.com>
2026-01-27 12:14:16 +0100

    ajout model pieces detachees pricing

---
+++ models/marts/nesp_tech/fct_nesp_tech__pieces_detachees_pricing.sql
@@ -0,0 +1,58 @@
+{{ config(
+    materialized = 'table',
+    description='Table de pricing de la consommation des pièces détachées'
+) }}
+
+-- Modèle : interventions_articles
+-- Objectif : ce modèle combine les interventions terminées/signées avec les articles consommés
+-- et leurs informations tarifaires, afin de calculer les montants totaux par article.
+-- Il permet d’obtenir un niveau de détail par intervention (n_planning) et article.
+
+with inters as (
+  -- Étape 1 : Sélection des interventions terminées ou signées, filtrées par les agences EVS
+  -- NB : on récupére les dates de fin de la partie Interventions car pour les Pièces, c'est la date de début qui fait foi.
+  select 
+    n_planning,
+    cast(date_heure_fin as date) as date_fin
+  from {{ref('stg_nesp_tech__interventions')}}
+  where etat_intervention in ('terminée signée','signature différée')
+    and agency in ('evs','evs paris','evs idf','evs paris 2')
+),
+final as (
+  -- Étape 2 : Jointure avec les consommations d’articles et leurs prix
+  select 
+    i.n_planning,
+    i.date_fin,
+    extract(year from i.date_fin) as annee,
+    extract(month from i.date_fin) as mois,
+    extract(day from i.date_fin) as jour,
+    n_tech,
+    nom_tech,
+    prenom_tech,
+    art_pricing.article_ref_nomad as piece_ref_nomad,
+    art_pricing.article_desc as piece_desc,
+    quantite_article as piece_quantite,
+    article_prix_unitaire as piece_prix_unitaire,
+    (quantite_article * article_prix_unitaire) as montant_total
+  from {{ref('stg_nesp_tech__articles')}} art_conso
+  left join {{ref('ref_nesp_tech__articles_prix')}} art_pricing
+    on art_conso.code_article = lower(art_pricing.article_ref_nomad)
+  inner join inters i 
+    on i.n_planning = art_conso.n_planning
+)
+-- Étape 3 : Sélection finale
+SELECT
+-- Info Interventions et Technicien
+n_planning,
+date_fin,
+n_tech,
+nom_tech,
+prenom_tech,
+-- Info Piece detachees
+piece_ref_nomad,
+piece_desc,
+piece_prix_unitaire,
+piece_quantite,
+-- Montant a facturer
+montant_total
+FROM final
