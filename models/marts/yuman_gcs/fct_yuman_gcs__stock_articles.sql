6e9ce0462f94d8074f4586d64173e61a9a7fbac0
CebrailEVS <cebrail.aksoy@evs-pro.com>
2026-02-10 07:36:34 +0000

    Fixed standardization marts model

--- models/marts/yuman_gcs/fct_yuman_gcs__stock_articles.sql
+++ models/marts/yuman_gcs/fct_yuman_gcs__stock_articles.sql
@@ -1,41 +1,30 @@
-{{
-  config(
+{{ config(
     materialized='table',
     description='Table de faits des stocks des pièces Yuman journaliers pour chaque stock technicien et dépôt'
-  )
-}}
+) }}
 
--- ============================================================================
--- MODEL: fct_yuman__stocks
--- PURPOSE: Suivis des stocks pièces Yuman par stock technicien et dépôt journaliers
--- AUTHOR: Cebrail AKSOY
--- ============================================================================
+with filtered_stocks as (
+    select
+        -- Attributs metier
+        reference,
+        designation,
+        nom_du_stock as stock,
 
-WITH 
--- ============================================================================
--- 1. BASE DATA EXTRACTION
--- ============================================================================
-filtered_stocks as (
+        -- Mesure
+        quantite,
 
-SELECT
-    -- Attributs métier 
-    reference, 
-    designation, 
-    nom_du_stock as stock,
+        -- Date
+        date(export_date) as stock_date,
 
-    -- Mesure
-    quantite, 
+        -- Metadonnees dbt
+        current_timestamp() as dbt_updated_at,
+        '{{ invocation_id }}' as dbt_invocation_id
 
-    -- Date
-    DATE(export_date) as stock_date,
-
-    -- Métadonnées d'exécution
-    CURRENT_TIMESTAMP() as dbt_updated_at,
-    '{{ invocation_id }}' as dbt_invocation_id
-
-    FROM {{ ref('stg_yuman_gcs__stock_theorique') }}
-    WHERE reference is not null and nom_du_stock is not null
+    from {{ ref('stg_yuman_gcs__stock_theorique') }}
+    where
+        reference is not null
+        and nom_du_stock is not null
 )
 
-SELECT * 
-FROM filtered_stocks
+select *
+from filtered_stocks
