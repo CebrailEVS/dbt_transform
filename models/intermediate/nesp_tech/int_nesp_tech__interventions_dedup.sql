d7538b51721e6b6d0e3cefbf397ad6aa57f9bc6d
EtienneEVS <etienne.boulinier@evs-pro.com>
2026-02-19 10:40:17 +0100

    view dedup articles et inters + table alerting conso pieces (#26)
    * ajout models intermediaire : intervnetion_dedup et articles_dedup et ajout model marts alerting conso_pieces_aguila
    
    * fix : modification models dedup view --> table

---
+++ models/intermediate/nesp_tech/int_nesp_tech__interventions_dedup.sql
@@ -0,0 +1,17 @@
+{{ config(
+    materialized = 'table'
+) }}
+-- Liste des interventions dédupliquées par la date de fin
+with ranked as (
+
+    select *
+    from {{ ref('stg_nesp_tech__interventions') }}
+
+    qualify ROW_NUMBER() over (
+        partition by n_planning
+        order by date_heure_fin desc, extracted_at desc
+    ) = 1
+
+)
+
+select * from ranked
