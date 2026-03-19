9fa4604ce64f5251d7697bf754d96b6eb1bcd88c
CebrailEVS <cebrail.aksoy@evs-pro.com>
2026-03-19 13:28:31 +0000

    fix: updated nesp_tech models

--- models/staging/nesp_tech/stg_nesp_tech__articles.sql
+++ models/staging/nesp_tech/stg_nesp_tech__articles.sql
@@ -56,7 +56,7 @@ cleaned_data as (
         end as date_intervention,
 
         -- Metadata
-        timestamp(extracted_at) as extracted_at,
+        safe.parse_timestamp('%Y-%m-%d %H:%M:%S%Ez', extracted_at) as extracted_at,
         source_file
 
     from source_data
